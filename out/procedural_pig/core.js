0fd019da5eea937b9aeb95d9923fc94c36aef7a6
Benjamin Zaporzan <btzaporz@lakeheadu.ca>
2015-12-01 00:19:43 -0500

    setup

--- out/procedural_pig/core.js
+++ out/procedural_pig/core.js
@@ -4,14 +4,14 @@ goog.require('cljs.core');
 goog.require('quil.middleware');
 goog.require('quil.core');
 procedural_pig.core.setup = (function setup(){
-quil.core.frame_rate.call(null,(30));
+quil.core.frame_rate.call(null,(60));
 
 quil.core.color_mode.call(null,new cljs.core.Keyword(null,"hsb","hsb",-753472031));
 
 return new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"color","color",1011675173),(0),new cljs.core.Keyword(null,"angle","angle",1622094254),(0)], null);
 });
 procedural_pig.core.update_state = (function update_state(state){
-return new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"color","color",1011675173),cljs.core.mod.call(null,(new cljs.core.Keyword(null,"color","color",1011675173).cljs$core$IFn$_invoke$arity$1(state) + 0.7),(255)),new cljs.core.Keyword(null,"angle","angle",1622094254),(new cljs.core.Keyword(null,"angle","angle",1622094254).cljs$core$IFn$_invoke$arity$1(state) + 0.1)], null);
+return new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"color","color",1011675173),cljs.core.mod.call(null,(new cljs.core.Keyword(null,"color","color",1011675173).cljs$core$IFn$_invoke$arity$1(state) + 0.7),(255)),new cljs.core.Keyword(null,"angle","angle",1622094254),(new cljs.core.Keyword(null,"angle","angle",1622094254).cljs$core$IFn$_invoke$arity$1(state) + 0.05)], null);
 });
 procedural_pig.core.draw_state = (function draw_state(state){
 quil.core.background.call(null,(13));
@@ -19,25 +19,16 @@ quil.core.background.call(null,(13));
 quil.core.fill.call(null,new cljs.core.Keyword(null,"color","color",1011675173).cljs$core$IFn$_invoke$arity$1(state),(255),(255));
 
 var angle = new cljs.core.Keyword(null,"angle","angle",1622094254).cljs$core$IFn$_invoke$arity$1(state);
-var x = ((150) * quil.core.cos.call(null,angle));
-var y = ((150) * quil.core.sin.call(null,angle));
-var tr__5765__auto__ = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(quil.core.width.call(null) / (2)),(quil.core.height.call(null) / (2))], null);
+var x = ((75) * quil.core.cos.call(null,angle));
+var y = ((75) * quil.core.sin.call(null,angle));
+var tr__5782__auto__ = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(quil.core.width.call(null) / (2)),(quil.core.height.call(null) / (2))], null);
 quil.core.push_matrix.call(null);
 
-quil.core.translate.call(null,tr__5765__auto__);
+try{quil.core.translate.call(null,tr__5782__auto__);
 
-quil.core.ellipse.call(null,x,y,(100),(100));
-
-return quil.core.pop_matrix.call(null);
+return quil.core.ellipse.call(null,x,y,(100),(100));
+}finally {quil.core.pop_matrix.call(null);
+}});
+procedural_pig.core.start_ellipse_example = (function start_ellipse_example(){
+return quil.core.sketch.call(null,new cljs.core.Keyword(null,"host","host",-1558485167),"procedural-pig",new cljs.core.Keyword(null,"size","size",1098693007),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [window.innerWidth,window.innerHeight], null),new cljs.core.Keyword(null,"setup","setup",1987730512),procedural_pig.core.setup,new cljs.core.Keyword(null,"update","update",1045576396),procedural_pig.core.update_state,new cljs.core.Keyword(null,"draw","draw",1358331674),procedural_pig.core.draw_state,new cljs.core.Keyword(null,"middleware","middleware",1462115504),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [quil.middleware.fun_mode], null));
 });
-procedural_pig.core.procedural_pig = (function procedural_pig__$1(){
-return quil.sketch.sketch.call(null,new cljs.core.Keyword(null,"draw","draw",1358331674),procedural_pig.core.draw_state,new cljs.core.Keyword(null,"middleware","middleware",1462115504),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [quil.middleware.fun_mode], null),new cljs.core.Keyword(null,"setup","setup",1987730512),procedural_pig.core.setup,new cljs.core.Keyword(null,"size","size",1098693007),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [window.innerWidth,window.innerHeight], null),new cljs.core.Keyword(null,"update","update",1045576396),procedural_pig.core.update_state,new cljs.core.Keyword(null,"host","host",-1558485167),"procedural-pig");
-});
-goog.exportSymbol('procedural_pig.core.procedural_pig', procedural_pig.core.procedural_pig);
-
-if(cljs.core.truth_(cljs.core.some.call(null,(function (p1__5306__5307__auto__){
-return cljs.core._EQ_.call(null,new cljs.core.Keyword(null,"no-start","no-start",1381488856),p1__5306__5307__auto__);
-}),null))){
-} else {
-quil.sketch.add_sketch_to_init_list.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"fn","fn",-1175266204),procedural_pig.core.procedural_pig,new cljs.core.Keyword(null,"host-id","host-id",742376279),"procedural-pig"], null));
-}
